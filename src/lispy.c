3a415c812ff65e9309b3a4b565552bc8145d6d74
Adriano Del Vigna <adriano.delvigna@gmail.com>
2020-09-11 22:43:30 -0300

    Adds a mpc parser.
    For a simple polish notation calculator.

--- src/lispy.c
+++ src/lispy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "version.h"
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -28,6 +29,18 @@ void add_history(char *unused) {}
 
 int main(int argc, char const *argv[])
 {
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "number     : /-?[0-9]+/ ;"
+        "operator   : '+' | '-' | '*' | '/' ;"
+        "expr       : <number> | '(' <operator> <expr>+ ')' ;"
+        "lispy      : /^/ <operator> <expr>+ /$/ ;"
+    , Number, Operator, Expr, Lispy);
+
     printf("Lispy version %d.%d.%d\n", Lispy_VERSION_MAJOR, Lispy_VERSION_MINOR, Lispy_VERSION_PATCH);
     puts("Press Ctrl+c to exit\n");
 
@@ -35,10 +48,24 @@ int main(int argc, char const *argv[])
     {
         char *input = readline("lispy> ");
         add_history(input);
-        printf("You are a %s\n", input);
+
+        mpc_result_t result;
+        if (mpc_parse("<stdin>", input, Lispy, &result))
+        {
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        }
+        else 
+        {
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
+
         free(input);
         input = NULL;
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
